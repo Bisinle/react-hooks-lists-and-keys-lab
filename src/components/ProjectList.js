5f890d5b536c1c55da4216e227d4863c0590d39b
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-17 20:47:03 +0300

    mastering List and Keys, passed all tests

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,23 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const user = projects.map((user) => {
+    return (
+      <li key={user.id}>
+        {" "}
+        <ProjectItem
+          name={user.name}
+          about={user.about}
+          technologies={user.technologies}
+        />
+      </li>
+    );
+  });
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+
+      <div id="project-list">{user}</div>
     </div>
   );
 }
